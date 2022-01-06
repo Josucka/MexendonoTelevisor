8f219e8878d1b42b413a0da0052a18ec38efda23
Josué Barros <frajolla27@gmail.com>
2022-01-06 13:22:02 -0300

    <feat>(replicating Pong project, using moon language)

---
+++ Pong/main.lua
@@ -0,0 +1,40 @@
+--[[
+  Pong Remake 2022
+
+  --main program --
+
+  Autor: Josue Barros
+  frajolla27@gmail.com
+
+  Originally programmed by Atari in 1972. Features two paddles, controlled by players, with the goal of getting the ball past your opponent's edgs. First to 10 points wins.
+
+  This version is built to more closely resemble the NES than the original Pong machines or the Atari 2600 in terms of resolution, thouh in widescreen (16:9) so it looks nicer on modern systems.
+
+]]
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+--[[
+  Runs when the game first starts up, only once; used to initialize the game.
+]]
+function love.load()
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
+end
+
+--[[
+  Callde after update by LOVE2D, used to draw anything to the screen, update or otherwise.
+]]
+function love.draw()
+  love.graphics.printf(
+    'Hello Pong!',         -- text to render
+    0,                     -- starting X (0 since we're going to center it based on width)
+    WINDOW_HEIGHT / 2 - 6, -- starting Y (halfway down the screen)
+    WINDOW_WIDTH,          -- number of pixels to center withing (the entire screen here)
+    'center'               -- alignment mode, can be 'center', 'left' or 'right'
+  )
+end
